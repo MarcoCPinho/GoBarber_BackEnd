0c596a0f81b25cc8b2b7b3f2dffc7fcf8c6fb7db
MarcoCPinho <marcocpinho@hotmail.com>
2020-09-12 12:36:14 -0300

    changes on for using the db
    create an new method and values for the uses of the database (creating, saving and listings itens on it)

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,6 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity('aapointments')
+@Entity('appointments')
 class Appointment {
     @PrimaryGeneratedColumn('uuid')
     id: string;
