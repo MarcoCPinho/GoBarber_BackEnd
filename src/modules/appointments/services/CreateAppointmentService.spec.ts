7074dd44e6fc68ec8fcb045b4d9cd95c75a05990
MarcoCPinho <marcocpinho@hotmail.com>
2020-12-03 19:15:42 -0300

    Iniciando arquivo de testes

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,9 @@
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+    it('should be able to create a new appointment', () => {});
+});
+
+describe('CreateAppointment', () => {
+    it('should be not able to create two appointments on same date', () => {});
+});
