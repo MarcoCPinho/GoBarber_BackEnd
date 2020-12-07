42039ce1699960b9f4ae70176a12bd7e6d178503
MarcoCPinho <marcocpinho@hotmail.com>
2020-12-07 19:11:52 -0300

    updates on updateAvatarTest

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,57 +1,45 @@
-import path from 'path';
-import fs from 'fs';
 import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
-import uploadConfig from '@config/upload';
+import IStorageProvider from 'shared/container/providers/StorageProvider/models/IStorageProvider';
 import User from '../infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
 
 interface IRequestDTO {
-    user_id: string;
-    avatarFilename: string;
+  user_id: string;
+  avatarFilename: string;
 }
 
 @injectable()
 class UpdateUserAvatarService {
-    constructor(
-        @inject('usersRepository')
-        private usersRepository: IUsersRepository,
-    ) {}
+  constructor(
+    @inject('usersRepository')
+    private usersRepository: IUsersRepository,
 
-    public async execute({
-        user_id,
-        avatarFilename,
-    }: IRequestDTO): Promise<User> {
-        const user = await this.usersRepository.findById(user_id);
-        if (!user) {
-            throw new AppError(
-                'Only authenticated users can change avatar.',
-                401,
-            );
-        }
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
+  ) {}
 
-        if (user.avatar) {
-            const userAvatarFilePath = path.join(
-                uploadConfig.directory,
-                user.avatar,
-            );
-            const userAvatarFileExists = await fs.promises.stat(
-                userAvatarFilePath,
-            );
-
-            if (userAvatarFileExists) {
-                await fs.promises.unlink(userAvatarFilePath);
-            }
-        }
-
-        user.avatar = avatarFilename;
-
-        await this.usersRepository.save(user);
-
-        return user;
+  public async execute({
+    user_id,
+    avatarFilename,
+  }: IRequestDTO): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+    if (!user) {
+      throw new AppError('Only authenticated users can change avatar.', 401);
     }
+
+    if (user.avatar) {
+      await this.storageProvider.deleteFile(user.avatar);
+    }
+    const fileName = await this.storageProvider.saveFile(avatarFilename);
+    user.avatar = fileName;
+
+    await this.usersRepository.save(user);
+
+    return user;
+  }
 }
 
 export default UpdateUserAvatarService;
