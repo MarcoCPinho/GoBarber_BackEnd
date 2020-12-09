f58458dfb371063a6e36e406a6197f251ebab8f3
MarcoCPinho <marcocpinho@hotmail.com>
2020-12-08 22:14:49 -0300

    2 aulas de testes

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -4,40 +4,40 @@ import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRe
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-    it('should be able to create a new appointment', async () => {
-        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
-        const createAppointment = new CreateAppointmentService(
-            fakeAppointmentsRepository,
-        );
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
 
-        const appointment = await createAppointment.execute({
-            date: new Date(),
-            provider_id: '123123123123',
-        });
-        expect(appointment).toHaveProperty('id');
-        expect(appointment.provider_id).toBe('123123123123');
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123123123',
     });
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123123123');
+  });
 });
 
 describe('CreateAppointment', () => {
-    it('should be not able to create two appointments on same date', async () => {
-        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
-        const createAppointment = new CreateAppointmentService(
-            fakeAppointmentsRepository,
-        );
+  it('should be not able to create two appointments on same date', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
 
-        const appointmentDate = new Date();
+    const appointmentDate = new Date();
 
-        await createAppointment.execute({
-            date: appointmentDate,
-            provider_id: '123123123123',
-        });
-
-        expect(
-            createAppointment.execute({
-                date: appointmentDate,
-                provider_id: '123123123123',
-            }),
-        ).rejects.toBeInstanceOf(AppError);
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123123123123',
     });
+
+    await expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123123123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,43 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+// import User from '../infra/typeorm/entities/User';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+
+import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
+
+interface IRequestDTO {
+  email: string;
+}
+
+@injectable()
+class SendForgotEmailPasswordService {
+  constructor(
+    @inject('usersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
+  ) {}
+
+  async execute({ email }: IRequestDTO): Promise<void> {
+    const checkUserExists = await this.usersRepository.findByEmail(email);
+
+    if (!checkUserExists) {
+      throw new AppError('User does not exists');
+    }
+
+    await this.userTokensRepository.generate(checkUserExists.id);
+
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido',
+    );
+  }
+}
+export default SendForgotEmailPasswordService;
