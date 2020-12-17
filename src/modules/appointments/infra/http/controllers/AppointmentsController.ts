a94d03d02baa22a6279f7cf715ad253fbae25844
MarcoCPinho <marcocpinho@hotmail.com>
2020-12-16 21:29:58 -0300

    1 aula

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -6,21 +6,18 @@ import { container } from 'tsyringe';
 import CreateAppointmentsService from '@modules/appointments/services/CreateAppointmentService';
 
 export default class AppointmentController {
-    public async create(
-        request: Request,
-        response: Response,
-    ): Promise<Response> {
-        const { provider_id, date } = request.body;
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { provider_id, date } = request.body;
 
-        const parsedDate = parseISO(date);
+    const parsedDate = parseISO(date);
 
-        const createAppointment = container.resolve(CreateAppointmentsService);
+    const createAppointment = container.resolve(CreateAppointmentsService);
 
-        const appointment = await createAppointment.execute({
-            date: parsedDate,
-            provider_id,
-        });
+    const appointment = await createAppointment.execute({
+      date: parsedDate,
+      provider_id,
+    });
 
-        return response.json(appointment);
-    }
+    return response.json(appointment);
+  }
 }
