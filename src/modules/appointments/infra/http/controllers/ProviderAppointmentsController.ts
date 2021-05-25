02fa3af3871d536066e072ffeb65927f62b0ad51
Marco Carvalho de Pinho <marcocpinho@hotmail.com>
2021-05-24 22:30:39 -0300

    update

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -4,10 +4,12 @@ import { container } from 'tsyringe';
 
 import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
 
+// GET (BODY): http://localhost:3333/rota?year=2020&month=5&day=20
+
 export default class ProviderAppointmentController {
   public async index(request: Request, response: Response): Promise<Response> {
     const provider_id = request.user.id;
-    const { day, month, year } = request.body;
+    const { day, month, year } = request.query;
 
     const createAppointment = container.resolve(
       ListProviderAppointmentsService,
@@ -15,9 +17,9 @@ export default class ProviderAppointmentController {
 
     const appointments = await createAppointment.execute({
       provider_id,
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     });
 
     return response.json(appointments);
